fe98af9b047ed5cf2f803f3fca2b97a35b74d624
Shashank Taliwal <shashanktaliwal07@gmail.com>
2022-05-11 22:54:01 +0530

    Backend started

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+app.get('/', (req, res) => {
+    res.json({ "statusCode": 200, message: "Welcome to DQ" });
+});
+
+
+
+app.listen(port, (req, res) => {
+    console.log("Welcome to Dequeue!!");
+    console.log("=====================");
+    console.log("-------------------------------");
+    console.log(`| Server running at port ${port} |`);
+    console.log("-------------------------------");
+});
