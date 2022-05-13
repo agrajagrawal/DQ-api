a915445604519491c6c624ade812efcfe8d85772
Shashank Taliwal <shashanktaliwal07@gmail.com>
2022-05-13 12:09:45 +0530

    Documentation added

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require('cors');
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUI = require('swagger-ui-express');
 require('dotenv').config();
 const ProductInfo = require('./models/ProductInfo')
 const app = express();
 const port = process.env.PORT || 5000;
-
-// app.use(cors);
 
 // MONGODB ATLAS INTEGRATION
 mongoose.connect(
@@ -19,14 +19,23 @@ mongoose.connect(
     })
 
 
-// HOME ROUTE FOR TESTING SERVER STATUS
+//Routes
+
+/**
+ * @swagger
+ * /:
+ *  get:
+ *    description: Home route to check server status
+ *    responses:
+ *      200:
+ *        description: Server up and running
+ *      404:
+ *        description: Server is down
+ */
 app.get("/", (req, res) => {
     res.json({ statusCode: 200, message: "Welcome to DQ" });
 });
 
-
-//This is an optional route that can be used in future in case of addition of new Products is required
-//CAN BE IGNORED FOR NOW
 app.post('/create', async (req, res) => {
     const barcodeId = req.query.barcodeID;
     const Name = req.query.Name;
@@ -45,9 +54,24 @@ app.post('/create', async (req, res) => {
     res.json({ statusCode: 200, data: newProduct });
 })
 
-
-//This route would give the product info
-app.get("/getProduct", async (req, res) => {
+/**
+ * @swagger
+ * /getProduct:
+ *  post:
+ *    description: API endpoint to get the details of the scanned product
+ *    parameters:
+ *      - name: barcodeID
+ *        description: scanned barcode Id
+ *        in: formData
+ *        required: true
+ *        type: string
+ *    responses:
+ *      200:
+ *        description: Product available
+ *      404:
+ *        description: Product Not available
+ */
+app.post("/getProduct", async (req, res) => {
     const barCodeID = req.query.barcodeID;
     try {
         const info = await ProductInfo.find({ barcodeId: barCodeID });
@@ -57,8 +81,6 @@ app.get("/getProduct", async (req, res) => {
     }
 });
 
-
-//Standard listen port
 app.listen(port, () => {
     console.log("Welcome to Dequeue!!");
     console.log("=====================");
@@ -66,3 +88,26 @@ app.listen(port, () => {
     console.log(`| Server running at port ${port} |`);
     console.log("-------------------------------");
 });
+
+
+
+//The following code is for the documentation purposes and can be ABSOLUTELY SKIPPED
+
+const swaggerOptions = {
+    swaggerDefinition: {
+        info: {
+            title: "Deque API",
+            description: "A nodeJS based API for Deque App",
+            contact: {
+                name: "Fast and Serious",
+            },
+            servers: ["http://localhost:5000","https://dequeserver.herokuapp.com"]
+        }
+    },
+    apis: ["app.js"]
+}
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
+
+
